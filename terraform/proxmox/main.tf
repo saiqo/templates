fcb86d71c2a9b0ce3a876ec5d713139bf1ce7d80
Saiqo <saiqouu@gmail.com>
2025-04-03 22:23:03 +0200

    Fix indentation

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -6,18 +6,18 @@ resource "proxmox_virtual_environment_vm" "vm-proxmox" {
     node_name   = var.target_node
 
     name        = "debian-test-${count.index + 1}"
-	tags 		= ["terraform", "debian"]
-	description = "Managed by terraform"
-	on_boot     = true
+    tags 		= ["terraform", "debian"]
+    description = "Managed by terraform"
+    on_boot     = true
 
     agent {
         enabled = true
     }
-	clone {
+    clone {
         vm_id = "8000" # ID du template à clone
         full = true
     }
-	cpu {
+    cpu {
         cores = "1"
         sockets = "2"
         type = "x86-64-v2-AES" # Utiliser "host" pour plus de performance
@@ -25,31 +25,31 @@ resource "proxmox_virtual_environment_vm" "vm-proxmox" {
     memory {
         dedicated = "2048"
     }
-	network_device {
-		model = "virtio"
+    network_device {
+        model = "virtio"
         bridge = "vmbr0"
     }
 
-	disk {
-		datastore_id = "local-lvm"
-		interface    = "scsi0"
-		size    = 30 # Taille en Giga, Facultatif. Si renseigné, doit être + grand que le disque du template(resize)
-	}
+    disk {
+        datastore_id = "local-lvm"
+        interface    = "scsi0"
+        size    = 30 # Taille en Giga, Facultatif. Si renseigné, doit être + grand que le disque du template(resize)
+    }
 
-	initialization {
+    initialization {
         ip_config {
             ipv4 {
                 address = "192.168.1.17${count.index + 1}/24"
                 gateway = "192.168.1.1"
             }
         }
-		dns {
-			servers = ["1.1.1.1", "8.8.8.8"]
-		}
+        dns {
+            servers = ["1.1.1.1", "8.8.8.8"]
+        }
         user_account {
             keys     = [var.ssh_key]
             username = "saiqo"
-			password = "toto"
+            password = "toto"
         }
     }
 }
